0d0ce3dd178644d5e3b27854f20049f52b231f88
kou031 <testpilot031@gmail.com>
2021-04-29 10:05:22 +0900

    ”modify_variables”

--- variables.tf
+++ variables.tf
@@ -1,21 +1,15 @@
 variable "aws_region" {
-  type    = string
-  default = "x"
+  type = string
 }
 variable "aws_az_1" {
-  type    = string
-  default = "xxx"
+  type = string
 }
 variable "aws_az_2" {
-  type    = string
-  default = "xxx"
+  type = string
 }
 variable "aws_az_3" {
-  type    = string
-  default = "xxx"
+  type = string
 }
 variable "aws_target_rds_instance_class" {
   type = string
-  # example db.m5.2xlarge
-  default = "xxx"
 }
